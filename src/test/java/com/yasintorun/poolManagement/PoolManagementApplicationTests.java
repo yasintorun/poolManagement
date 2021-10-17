4337e74bd6109155ebf81dea1dccdec52b56083f
yasintorun <yasintorun4680@gmail.com>
2021-10-17 13:36:18 +0300

    Project created

---
+++ src/test/java/com/yasintorun/poolManagement/PoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasintorun.poolManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
